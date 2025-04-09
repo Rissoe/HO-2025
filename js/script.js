dba66256b020d19fc56d59f9ae8eb2ed8e9371bf
Rissoe <brunsonruben@gmail.com>
2025-04-09 13:34:08 +0200

    added code for questions

--- js/script.js
+++ js/script.js
@@ -3,3 +3,18 @@ function fontToggle() {
     document.body.style.fontFamily = document.getElementById("fontButton").style.fontFamily;
     document.getElementById("fontButton").style.fontFamily = original;
 }
+
+function Formify() {
+    let score = 0
+    let value = document.forms["QuestionArticle"]["Q1"].value;
+    if (value == "right") { score++ };
+    value = document.forms["QuestionArticle"]["Q2"].value;
+    if (value == "right") { score++ };
+    value = document.forms["QuestionArticle"]["Q3"].value;
+    if (value == "right") { score++ };
+    value = document.forms["QuestionArticle"]["Q4"].value;
+    if (value == "right") { score++ };
+    value = document.forms["QuestionArticle"]["Q5"].value;
+    if (value == "right") { score++ };
+    alert(score+"/5 change to a message on the page later")
+}
