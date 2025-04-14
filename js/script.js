b4e30e0f6daaa895df286d67178c8a625490fe67
Rissoe <brunsonruben@gmail.com>
2025-04-14 16:35:26 +0200

    article page done

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ function fontToggle() {
 
 function Formify() {
     let score = 0
+    let message = ""
     let value = document.forms["QuestionArticle"]["Q1"].value;
     if (value == "right") { score++ };
     value = document.forms["QuestionArticle"]["Q2"].value;
@@ -16,5 +17,14 @@ function Formify() {
     if (value == "right") { score++ };
     value = document.forms["QuestionArticle"]["Q5"].value;
     if (value == "right") { score++ };
-    alert(score+"/5 change to a message on the page later")
+
+    if (score == 0) { message = "Heb je zelfs dingen ingevuld?" }
+    else if (score == 1) { message = "Dat kan beter." }
+    else if (score == 2) { message = "Net niet goed genoeg!" }
+    else if (score == 3) { message = "Wel minstens is het over de helft?"}
+    else if (score == 4) { message = "Dat is een goede score!" }
+    else { message = "Goed gedaan!" }
+    event.preventDefault()
+    alert(score+"/5\n"+message)
+    location.replace("../article.html")
 }
